b7ccde8d0d2798114bd3d8095958497bd0918b5c
przemyslaw <75281967+PRMK01@users.noreply.github.com>
2021-09-07 23:20:19 +0200

    Refactor code

--- script.js
+++ script.js
@@ -1,148 +1,172 @@
-const activatedButtons = document.querySelectorAll('.inactive');
-const topRow = document.querySelector('.screen-top-part');
-const bottomRow = document.querySelector('.screen-bottom-part');
-const memory = { active: false,value: '' }
-let expression = '';
-let bottomRowActive = true;
+const calculator = {
 
-function memoryUpdate (target) {
-    bottomRowActive = false;
-    memory.active = true;
-    for (button of activatedButtons) {
-        button.classList.remove('inactive');
-    }
-    switch (target.textContent) {
-        case 'MC': {
-            memory.value = '';
-            for (button of activatedButtons) {
-                button.classList.add('inactive');
+    expression: '',
+
+    bottomRowActive: true,
+
+    memoryData: {active: false, value: ''},
+
+    memory (target) {
+        calculator.bottomRowActive = false;
+        calculator.memoryData.active = true;
+        for (button of activatedButtons) {
+            button.classList.remove('inactive');
+        }
+        switch (target.textContent) {
+            case 'MC': {
+                calculator.memoryData.value = '';
+                for (button of activatedButtons) {
+                    button.classList.add('inactive');
+                }
+                break
+            } case 'MR': {
+                calculator.clear();
+                bottomRow.textContent = calculator.memoryData.value;
+                break
+            } case 'M+': {
+                calculator.memoryData.value = eval(+calculator.memoryData.value + +bottomRow.textContent).toString();
+                break
+            } case 'M-': {
+                calculator.memoryData.value = eval(+calculator.memoryData.value - +bottomRow.textContent).toString();
+                break
+            } case 'MS': {
+                calculator.memoryData.value = bottomRow.textContent;
+                break
             }
-            break
-        } case 'MR': {
-            clearEntry();
-            bottomRow.textContent = memory.value;
-            break
-        } case 'M+': {
-            memory.value = eval(+memory.value + +bottomRow.textContent).toString();
-            break
-        } case 'M-': {
-            memory.value = eval(+memory.value - +bottomRow.textContent).toString();
-            break
-        } case 'MS': {
-            memory.value = bottomRow.textContent;
-            break
         }
-    }
-}
+    },
 
-function appendNumber (target) {
-    if (bottomRowActive) {
-        if (bottomRow.textContent === '0') {
-            bottomRow.textContent = target.textContent;
-        } else if (!(bottomRow.textContent.length >= 15)) {
-            bottomRow.textContent = bottomRow.textContent + target.textContent;
-        }
-        return
-    }
-    if (topRow.textContent.includes('=')) {
-        topRow.textContent = '';
-        expression = '';
-    }
-    bottomRowActive = true;
-    bottomRow.textContent = target.textContent;
-}
-
-function clearAll () {
-    topRow.textContent = '';
-    bottomRow.textContent = '0';
-    expression = '';
-}
-
-function clearEntry () {
-    if (topRow.textContent.includes('=')) {
-        clearAll();
-    } else {
-        bottomRow.textContent = '0';
-    }
-}
-
-function deleteLast () {
-    if (memory.active) {
-        return
-    }
-    if (bottomRowActive) {
-        if (bottomRow.textContent == '0') {
+    number (target) {
+        if (calculator.bottomRowActive) {
+            if (bottomRow.textContent === '0') {
+                bottomRow.textContent = target.textContent;
+            } else if (!(bottomRow.textContent.length >= 15)) {
+                bottomRow.textContent = bottomRow.textContent + target.textContent;
+            }
             return
-        } else if ((bottomRow.textContent.length === 3 && bottomRow.textContent.includes('.') && bottomRow.textContent.includes('-')) || (bottomRow.textContent.length === 2 && bottomRow.textContent.includes('-')) || bottomRow.textContent.length === 1) {
+        }
+        if (topRow.textContent.includes('=')) {
+            topRow.textContent = '';
+            calculator.expression = '';
+        }
+        calculator.bottomRowActive = true;
+        bottomRow.textContent = target.textContent;
+    },
+
+    clearAll () {
+        topRow.textContent = '';
+        bottomRow.textContent = '0';
+        calculator.expression = '';
+    },
+
+    clear () {
+        if (topRow.textContent.includes('=')) {
+            calculator.clearAll();
+        } else {
             bottomRow.textContent = '0';
-        } else {
-            bottomRow.textContent = bottomRow.textContent.slice(0, bottomRow.textContent.length - 1)
         }
-    }
-}
+    },
 
-function basicOperation (target) {
-    if (bottomRowActive || memory.active) {
-        expression = `(${expression}(${bottomRow.textContent}))`; 
-        topRow.textContent = evaluate() + ' ' +  target.textContent;
-        expression += target.dataset.value;
-        bottomRowActive = false;
-        memory.active = false;
-    } else {
-        if (/\+|-|÷|×/.test(topRow.textContent.slice(-1))) {
-            topRow.textContent = topRow.textContent.slice(0, -1) + target.textContent;
-            expression = expression.slice(0, -1) + target.dataset.value;
-        } else {
-            topRow.textContent = evaluate() + ' ' +  target.textContent;
-            expression += target.dataset.value;
+    delete () {
+        if (calculator.memoryData.active) {
+            return
         }
-    }
-}
+        if (calculator.bottomRowActive) {
+            if (bottomRow.textContent == '0') {
+                return
+            } else if ((bottomRow.textContent.length === 3 && bottomRow.textContent.includes('.') && bottomRow.textContent.includes('-')) || (bottomRow.textContent.length === 2 && bottomRow.textContent.includes('-')) || bottomRow.textContent.length === 1) {
+                bottomRow.textContent = '0';
+            } else {
+                bottomRow.textContent = bottomRow.textContent.slice(0, bottomRow.textContent.length - 1)
+            }
+        }
+    },
+    
+    operator (target) {
+        if ((calculator.bottomRowActive || calculator.memoryData.active) && !(topRow.textContent.includes('='))) {
+            calculator.expression = `(${calculator.expression}(${bottomRow.textContent}))`; 
+            topRow.textContent = calculator.evaluate() + ' ' +  target.textContent;
+            calculator.expression += target.dataset.value;
+            calculator.bottomRowActive = false;
+            calculator.memoryData.active = false;
+        } else {
+            if (/\+|-|÷|×/.test(topRow.textContent.slice(-1))) {
+                topRow.textContent = topRow.textContent.slice(0, -1) + target.textContent;
+                calculator.expression = calculator.expression.slice(0, -1) + target.dataset.value;
+            } else {
+                topRow.textContent = calculator.evaluate() + ' ' +  target.textContent;
+                calculator.expression += target.dataset.value;
+            }
+        }
+    },
+    
+    equals () {
+        calculator.bottomRowActive = false;
+        calculator.memoryData.active = false;
+        if (topRow.textContent.includes('=')) {
+            return
+        }
+        if (bottomRow.textContent.slice(-1) === '.') {
+            bottomRow.textContent = bottomRow.textContent.slice(0, -1);
+        }
+        if (eval(bottomRow.textContent) === 0) {
+            topRow.textContent = `${topRow.textContent} 0 =`;
+        } else if (bottomRow.textContent.includes('-')) {
+            topRow.textContent = `${topRow.textContent} (${bottomRow.textContent}) =`;
+        } else {
+            topRow.textContent = `${topRow.textContent} ${bottomRow.textContent} =`;
+        }
+        calculator.expression = `(${calculator.expression}(${bottomRow.textContent}))`;
+        bottomRow.textContent = calculator.evaluate();
+    },
+    
+    decimal () {
+        if (!calculator.bottomRowActive) {
+            if (topRow.textContent.includes('=')) {
+                topRow.textContent = '';
+                calculator.expression = '';
+            }
+            bottomRow.textContent = '0.';
+            calculator.bottomRowActive = true;
+        } else if (!(bottomRow.textContent.length >= 14) && !bottomRow.textContent.includes('.')) {
+            bottomRow.textContent += '.';
+        }
+    },
+    
+    minus () {
+        if (bottomRow.textContent === '0') { return }
+        if (bottomRow.textContent.includes('-')) {
+            bottomRow.textContent = bottomRow.textContent.slice(1, bottomRow.textContent.length);
+        } else {
+            bottomRow.textContent = '-' + bottomRow.textContent;
+        }
+    },
+    
+    evaluate () {
+        let result = eval(calculator.expression);
+        let resultRounded = +parseFloat(result).toFixed(8);
+        if (resultRounded.toString().length >= 18) {
+            resultRounded = resultRounded.toExponential();
+        }
+        resultRounded = resultRounded.toString();
+        if (resultRounded.includes('e')) {
+            let number = resultRounded.split('e')[0];
+            let numberRounded = (+parseFloat(number).toFixed(8)).toString();
+            resultRounded = numberRounded + 'e' + resultRounded.split('e')[1];
+        }
+        return resultRounded;
+    },
 
-function equals () {
-    bottomRowActive = false;
-    memory.active = false;
-    if (topRow.textContent.includes('=')) {
-        return
+    buttonClicked (e) {
+    calculator[e.target.dataset.functionName](e.target)
     }
-    if (bottomRow.textContent.includes('-')) {
-        topRow.textContent = `${topRow.textContent} (${bottomRow.textContent}) =`;
-    } else {
-        topRow.textContent = `${topRow.textContent} ${bottomRow.textContent} =`;
-    }
-    expression = `(${expression}(${bottomRow.textContent}))`;
-    bottomRow.textContent = evaluate();
-}
+};
 
-function decimalPoint () {
-    if (!bottomRowActive) {
-        bottomRow.textContent = '0.';
-        bottomRowActive = true;
-    } else if (!(bottomRow.textContent.length >= 14) && !bottomRow.textContent.includes('.')) {
-        bottomRow.textContent += '.';
-    }
-}
+const buttons = document.querySelectorAll('.button');
+const activatedButtons = document.querySelectorAll('.inactive');
+const topRow = document.querySelector('#screen-top-part');
+const bottomRow = document.querySelector('#screen-bottom-part');
 
-function minus () {
-    if (bottomRow.textContent === '0') { return }
-    if (bottomRow.textContent.includes('-')) {
-        bottomRow.textContent = bottomRow.textContent.slice(1, bottomRow.textContent.length);
-    } else {
-        bottomRow.textContent = '-' + bottomRow.textContent;
-    }
-}
-
-function evaluate () {
-    let result = eval(expression);
-    let resultRounded = +parseFloat(result).toFixed(8);
-    if (resultRounded.toString().length >= 18) {
-        resultRounded = resultRounded.toExponential();
-    }
-    resultRounded = resultRounded.toString();
-    if (resultRounded.includes('e')) {
-        let number = resultRounded.split('e')[0];
-        let numberRounded = (+parseFloat(number).toFixed(8)).toString();
-        resultRounded = numberRounded + 'e' + resultRounded.split('e')[1];
-    }
-    return resultRounded;
-}
+buttons.forEach(button => {
+    button.addEventListener('click', calculator.buttonClicked)
+});
---
+++ visuals.js
@@ -0,0 +1,40 @@
+(() => {
+    let debounceTimeout;
+
+    document.addEventListener('keyup', handlekeys)
+
+    function handlekeys (ev) {
+        for (button of buttons) {
+            if (button.innerText == ev.key || button.dataset.value == ev.key) {
+                button.click();
+            }
+        }
+    }
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            button.focus();
+            button.style.outline = 'none';   
+            clickVisual(button);
+        });
+        button.addEventListener('blur', () => {
+            button.style.outline = 'none';
+        });
+        button.addEventListener('focus', () => {
+            button.style.outline = 'solid 2px rgba(255, 255, 255, 0.3)';
+        });
+    })
+
+    function clickVisual (button) {
+        clearTimeout(debounceTimeout);
+        debounceTimeout = setTimeout(() => {
+            buttons.forEach(button => {
+                button.classList.remove('active');
+            })
+        }, 40);
+        button.classList.add('active');
+        setTimeout(() => {
+            button.classList.remove('active');
+        }, 40);
+    }
+})();
